1fe34edd9e04f887c96ecbf5f3a7cadc2bd6ee8f
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-01-02 00:48:43 +0100

    fix parry indicator render overlap

--- 1.21.3/Common/src/main/java/fuzs/swordblockingmechanics/client/SwordBlockingMechanicsClient.java
+++ 1.21.3/Common/src/main/java/fuzs/swordblockingmechanics/client/SwordBlockingMechanicsClient.java
@@ -2,6 +2,7 @@ package fuzs.swordblockingmechanics.client;
 
 import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
 import fuzs.puzzleslib.api.client.event.v1.entity.player.MovementInputUpdateCallback;
+import fuzs.puzzleslib.api.client.event.v1.gui.RenderGuiEvents;
 import fuzs.puzzleslib.api.client.event.v1.gui.RenderGuiLayerEvents;
 import fuzs.puzzleslib.api.client.event.v1.renderer.ExtractRenderStateCallbackV2;
 import fuzs.puzzleslib.api.client.event.v1.renderer.RenderHandEvents;
@@ -17,16 +18,14 @@ public class SwordBlockingMechanicsClient implements ClientModConstructor {
     }
 
     private static void registerEventHandlers() {
-        RenderHandEvents.BOTH.register(FirstPersonRenderingHandler::onRenderBothHands);
-        MovementInputUpdateCallback.EVENT.register(MovementSlowdownHandler::onMovementInputUpdate);
-        RenderGuiLayerEvents.before(RenderGuiLayerEvents.CROSSHAIR)
-                .register(AttackIndicatorInGuiHandler::onBeforeRenderGuiLayer);
+        RenderGuiEvents.BEFORE.register(AttackIndicatorInGuiHandler::onBeforeRenderGui);
+        RenderGuiEvents.AFTER.register(AttackIndicatorInGuiHandler::onAfterRenderGui);
         RenderGuiLayerEvents.after(RenderGuiLayerEvents.CROSSHAIR)
                 .register(AttackIndicatorInGuiHandler.onAfterRenderGuiLayer(RenderGuiLayerEvents.CROSSHAIR));
-        RenderGuiLayerEvents.before(RenderGuiLayerEvents.HOTBAR)
-                .register(AttackIndicatorInGuiHandler::onBeforeRenderGuiLayer);
         RenderGuiLayerEvents.after(RenderGuiLayerEvents.HOTBAR)
                 .register(AttackIndicatorInGuiHandler.onAfterRenderGuiLayer(RenderGuiLayerEvents.HOTBAR));
+        RenderHandEvents.BOTH.register(FirstPersonRenderingHandler::onRenderBothHands);
+        MovementInputUpdateCallback.EVENT.register(MovementSlowdownHandler::onMovementInputUpdate);
         ExtractRenderStateCallbackV2.EVENT.register(FirstPersonRenderingHandler::onExtractRenderState);
     }
 }
--- 1.21.3/Common/src/main/java/fuzs/swordblockingmechanics/client/handler/AttackIndicatorInGuiHandler.java
+++ 1.21.3/Common/src/main/java/fuzs/swordblockingmechanics/client/handler/AttackIndicatorInGuiHandler.java
@@ -1,7 +1,6 @@
 package fuzs.swordblockingmechanics.client.handler;
 
 import fuzs.puzzleslib.api.client.event.v1.gui.RenderGuiLayerEvents;
-import fuzs.puzzleslib.api.event.v1.core.EventResult;
 import fuzs.swordblockingmechanics.SwordBlockingMechanics;
 import fuzs.swordblockingmechanics.config.ClientConfig;
 import fuzs.swordblockingmechanics.handler.SwordBlockingHandler;
@@ -20,13 +19,19 @@ public class AttackIndicatorInGuiHandler {
     @Nullable
     private static AttackIndicatorStatus attackIndicator = null;
 
-    public static EventResult onBeforeRenderGuiLayer(Gui gui, GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
-        if (!SwordBlockingMechanics.CONFIG.get(ClientConfig.class).renderParryIndicator) return EventResult.PASS;
+    public static void onBeforeRenderGui(Gui gui, GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
+        if (!SwordBlockingMechanics.CONFIG.get(ClientConfig.class).renderParryIndicator) return;
         if (attackIndicator == null && SwordBlockingHandler.getParryStrengthScale(gui.minecraft.player) != 0.0) {
             attackIndicator = gui.minecraft.options.attackIndicator().get();
             gui.minecraft.options.attackIndicator().set(AttackIndicatorStatus.OFF);
         }
-        return EventResult.PASS;
+    }
+
+    public static void onAfterRenderGui(Gui gui, GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
+        if (attackIndicator != null) {
+            gui.minecraft.options.attackIndicator().set(attackIndicator);
+            attackIndicator = null;
+        }
     }
 
     public static RenderGuiLayerEvents.After onAfterRenderGuiLayer(ResourceLocation resourceLocation) {
@@ -38,20 +43,29 @@ public class AttackIndicatorInGuiHandler {
     public static void onAfterRenderGuiLayer(ResourceLocation resourceLocation, Gui gui, GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
         // reset to old value; don't just leave this disabled as it'll change the vanilla setting permanently in options.txt, which no mod should do imo
         if (attackIndicator != null) {
-            gui.minecraft.options.attackIndicator().set(attackIndicator);
-            attackIndicator = null;
             int screenWidth = guiGraphics.guiWidth();
             int screenHeight = guiGraphics.guiHeight();
             double parryStrengthScale = Math.abs(SwordBlockingHandler.getParryStrengthScale(gui.minecraft.player));
-            if (resourceLocation.equals(RenderGuiLayerEvents.CROSSHAIR) && gui.minecraft.options.attackIndicator().get() == AttackIndicatorStatus.CROSSHAIR) {
+            if (resourceLocation.equals(RenderGuiLayerEvents.CROSSHAIR) &&
+                    attackIndicator == AttackIndicatorStatus.CROSSHAIR) {
                 if (gui.minecraft.options.getCameraType().isFirstPerson()) {
                     int posX = screenWidth / 2 - 8;
                     int posY = screenHeight / 2 - 7 + 16;
                     int textureHeight = (int) (parryStrengthScale * 15.0);
                     guiGraphics.blit(RenderType::crosshair, GUI_ICONS_LOCATION, posX, posY, 54, 0, 16, 14, 256, 256);
-                    guiGraphics.blit(RenderType::crosshair, GUI_ICONS_LOCATION, posX, posY + 14 - textureHeight, 70, 14 - textureHeight, 16, textureHeight, 256, 256);
+                    guiGraphics.blit(RenderType::crosshair,
+                            GUI_ICONS_LOCATION,
+                            posX,
+                            posY + 14 - textureHeight,
+                            70,
+                            14 - textureHeight,
+                            16,
+                            textureHeight,
+                            256,
+                            256);
                 }
-            } else if (resourceLocation.equals(RenderGuiLayerEvents.HOTBAR) && gui.minecraft.options.attackIndicator().get() == AttackIndicatorStatus.HOTBAR) {
+            } else if (resourceLocation.equals(RenderGuiLayerEvents.HOTBAR) &&
+                    attackIndicator == AttackIndicatorStatus.HOTBAR) {
                 int posX;
                 if (gui.minecraft.player.getMainArm() == HumanoidArm.LEFT) {
                     posX = screenWidth / 2 - 91 - 22;
@@ -61,7 +75,16 @@ public class AttackIndicatorInGuiHandler {
                 int posY = screenHeight - 20;
                 int textureHeight = (int) (parryStrengthScale * 19.0F);
                 guiGraphics.blit(RenderType::guiTextured, GUI_ICONS_LOCATION, posX, posY, 0, 0, 18, 18, 256, 256);
-                guiGraphics.blit(RenderType::guiTextured, GUI_ICONS_LOCATION, posX, posY + 18 - textureHeight, 18, 18 - textureHeight, 18, textureHeight, 256, 256);
+                guiGraphics.blit(RenderType::guiTextured,
+                        GUI_ICONS_LOCATION,
+                        posX,
+                        posY + 18 - textureHeight,
+                        18,
+                        18 - textureHeight,
+                        18,
+                        textureHeight,
+                        256,
+                        256);
             }
         }
     }
