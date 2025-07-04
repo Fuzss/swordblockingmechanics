c149588591cd13ecf4d44fd0eb33bf219e12f082
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-07-04 11:58:24 +0200

    full 1.21.7 port

--- 1.21.7/Common/src/main/java/fuzs/swordblockingmechanics/client/handler/AttackIndicatorInGuiHandler.java
+++ 1.21.7/Common/src/main/java/fuzs/swordblockingmechanics/client/handler/AttackIndicatorInGuiHandler.java
@@ -1,16 +1,17 @@
 package fuzs.swordblockingmechanics.client.handler;
 
-import fuzs.puzzleslib.api.client.event.v1.gui.RenderGuiLayerEvents;
 import fuzs.swordblockingmechanics.SwordBlockingMechanics;
 import fuzs.swordblockingmechanics.config.ClientConfig;
 import fuzs.swordblockingmechanics.handler.SwordBlockingHandler;
 import net.minecraft.client.AttackIndicatorStatus;
 import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.HumanoidArm;
+import net.minecraft.world.entity.player.Player;
 import org.jetbrains.annotations.Nullable;
 
 public class AttackIndicatorInGuiHandler {
@@ -34,58 +35,52 @@ public class AttackIndicatorInGuiHandler {
         }
     }
 
-    public static RenderGuiLayerEvents.After onAfterRenderGuiLayer(ResourceLocation resourceLocation) {
-        return (Gui gui, GuiGraphics guiGraphics, DeltaTracker deltaTracker) -> {
-            onAfterRenderGuiLayer(resourceLocation, gui, guiGraphics, deltaTracker);
-        };
-    }
-
-    public static void onAfterRenderGuiLayer(ResourceLocation resourceLocation, Gui gui, GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
-        // reset to old value; don't just leave this disabled as it'll change the vanilla setting permanently in options.txt, which no mod should do imo
-        if (attackIndicator != null) {
-            int screenWidth = guiGraphics.guiWidth();
-            int screenHeight = guiGraphics.guiHeight();
-            double parryStrengthScale = Math.abs(SwordBlockingHandler.getParryStrengthScale(gui.minecraft.player));
-            if (resourceLocation.equals(RenderGuiLayerEvents.CROSSHAIR) &&
-                    attackIndicator == AttackIndicatorStatus.CROSSHAIR) {
-                if (gui.minecraft.options.getCameraType().isFirstPerson()) {
-                    int posX = screenWidth / 2 - 8;
-                    int posY = screenHeight / 2 - 7 + 16;
-                    int textureHeight = (int) (parryStrengthScale * 15.0);
-                    guiGraphics.blit(RenderType::crosshair, GUI_ICONS_LOCATION, posX, posY, 54, 0, 16, 14, 256, 256);
-                    guiGraphics.blit(RenderType::crosshair,
-                            GUI_ICONS_LOCATION,
-                            posX,
-                            posY + 14 - textureHeight,
-                            70,
-                            14 - textureHeight,
-                            16,
-                            textureHeight,
-                            256,
-                            256);
-                }
-            } else if (resourceLocation.equals(RenderGuiLayerEvents.HOTBAR) &&
-                    attackIndicator == AttackIndicatorStatus.HOTBAR) {
-                int posX;
-                if (gui.minecraft.player.getMainArm() == HumanoidArm.LEFT) {
-                    posX = screenWidth / 2 - 91 - 22;
-                } else {
-                    posX = screenWidth / 2 + 91 + 6;
-                }
-                int posY = screenHeight - 20;
-                int textureHeight = (int) (parryStrengthScale * 19.0F);
-                guiGraphics.blit(RenderType::guiTextured, GUI_ICONS_LOCATION, posX, posY, 0, 0, 18, 18, 256, 256);
-                guiGraphics.blit(RenderType::guiTextured,
+    public static void renderCrosshairBlockingIndicator(GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
+        if (attackIndicator == AttackIndicatorStatus.CROSSHAIR) {
+            Minecraft minecraft = Minecraft.getInstance();
+            if (minecraft.options.getCameraType().isFirstPerson()) {
+                int posX = guiGraphics.guiWidth() / 2 - 8;
+                int posY = guiGraphics.guiHeight() / 2 - 7 + 16;
+                double parryStrengthScale = SwordBlockingHandler.getParryStrengthScale(minecraft.player);
+                int textureHeight = (int) (Math.abs(parryStrengthScale) * 15.0);
+                guiGraphics.blit(RenderPipelines.CROSSHAIR, GUI_ICONS_LOCATION, posX, posY, 54, 0, 16, 14, 256, 256);
+                guiGraphics.blit(RenderPipelines.CROSSHAIR,
                         GUI_ICONS_LOCATION,
                         posX,
-                        posY + 18 - textureHeight,
-                        18,
-                        18 - textureHeight,
-                        18,
+                        posY + 14 - textureHeight,
+                        70,
+                        14 - textureHeight,
+                        16,
                         textureHeight,
                         256,
                         256);
             }
+        }
+    }
+
+    public static void renderHotbarBlockingIndicator(GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
+        if (attackIndicator == AttackIndicatorStatus.HOTBAR) {
+            Player player = Minecraft.getInstance().player;
+            int posX;
+            if (player.getMainArm() == HumanoidArm.LEFT) {
+                posX = guiGraphics.guiWidth() / 2 - 91 - 22;
+            } else {
+                posX = guiGraphics.guiWidth() / 2 + 91 + 6;
+            }
+            int posY = guiGraphics.guiHeight() - 20;
+            double parryStrengthScale = SwordBlockingHandler.getParryStrengthScale(player);
+            int textureHeight = (int) (Math.abs(parryStrengthScale) * 19.0F);
+            guiGraphics.blit(RenderPipelines.GUI_TEXTURED, GUI_ICONS_LOCATION, posX, posY, 0, 0, 18, 18, 256, 256);
+            guiGraphics.blit(RenderPipelines.GUI_TEXTURED,
+                    GUI_ICONS_LOCATION,
+                    posX,
+                    posY + 18 - textureHeight,
+                    18,
+                    18 - textureHeight,
+                    18,
+                    textureHeight,
+                    256,
+                    256);
         }
     }
 }
--- 1.21.7/NeoForge/src/main/java/fuzs/swordblockingmechanics/neoforge/data/ModSoundDefinitionProvider.java
+++ 1.21.7/NeoForge/src/main/java/fuzs/swordblockingmechanics/neoforge/data/ModSoundDefinitionProvider.java
@@ -2,6 +2,7 @@ package fuzs.swordblockingmechanics.neoforge.data;
 
 import fuzs.puzzleslib.api.data.v2.core.DataProviderContext;
 import fuzs.puzzleslib.neoforge.api.client.data.v2.AbstractSoundProvider;
+import fuzs.swordblockingmechanics.SwordBlockingMechanics;
 import fuzs.swordblockingmechanics.init.ModRegistry;
 
 public class ModSoundDefinitionProvider extends AbstractSoundProvider {
@@ -13,7 +14,7 @@ public class ModSoundDefinitionProvider extends AbstractSoundProvider {
     @Override
     public void addSounds() {
         this.add(ModRegistry.ITEM_SWORD_BLOCK_SOUND_EVENT.value(),
-                sound(this.id("item/sword/block1")).volume(0.8),
-                sound(this.id("item/sword/block2")).volume(0.8));
+                sound(SwordBlockingMechanics.id("item/sword/block1")).volume(0.8),
+                sound(SwordBlockingMechanics.id("item/sword/block2")).volume(0.8));
     }
 }
